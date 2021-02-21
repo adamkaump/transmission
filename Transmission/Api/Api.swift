14c7317f673386acf7bc8816b2f47015cf28d992
Adam Kaump <adam.kaump@gmail.com>
2021-02-20 22:26:29 -0500

    dynamic session id headers

--- Transmission/Api/Api.swift
+++ Transmission/Api/Api.swift
@@ -9,23 +9,59 @@ import Foundation
 
 struct Api {
     static func allTorrents(completion: @escaping (TorrentsContainer?) -> Void) {
+        
         let urlString = "http://192.168.1.13:9091/transmission/rpc"
-        let url = URL(string: urlString)!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("LtNdAFymX71Fok7F9W8CHt0ujndWDeNRr5j5cuOTXwNxhJVh", forHTTPHeaderField: "X-Transmission-Session-Id")
-        request.httpBody = body
-        URLSession.shared.dataTask(with: request) { data, response, error in
+        self.req(urlString: urlString, method: "POST", body: standardBody) { data, response, error in
             if let data = data, let container = try? JSONDecoder().decode(TorrentsContainer.self, from: data) {
                 DispatchQueue.main.async {
                     completion(container)
                 }
             }
             completion(nil)
+        }
+    }
+    
+    static func sessionId(completion: @escaping (String?) -> Void) {
+        let url = URL(string: "http://192.168.1.13:9091/transmission/rpc")!
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            let sessionId = (response as? HTTPURLResponse)?.value(forHTTPHeaderField: "X-Transmission-Session-Id")
+            completion(sessionId)
         }.resume()
     }
     
-    static var body: Data? {
+    static func req(urlString: String, method: String, body: Data?, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        request.httpBody = body
+        
+        print(method + " " + urlString)
+        
+        Api.sessionId { sessionId in
+            
+            guard let sessionId = sessionId else {
+                print("unable to get session id")
+                completion(nil, nil, NSError())
+                return
+            }
+            
+            request.addValue(sessionId, forHTTPHeaderField: "X-Transmission-Session-Id")
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                
+                if let code = (response as? HTTPURLResponse)?.statusCode {
+                    print(code)
+                }
+                
+                completion(data, response, error)
+            }.resume()
+        }
+    }
+}
+
+extension Api {
+    static var standardBody: Data? {
         var body = [String: Any]()
         body["method"] = "torrent-get"
         
